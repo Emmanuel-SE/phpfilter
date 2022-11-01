3b3b460501b37651f7ad8f7c2e7fe78504e7711f
Emmanuel-SE <emmanueltiti370@gmail.com>
2022-11-01 20:43:43 +0300

    routing

---
+++ views/404.php
@@ -0,0 +1,10 @@
+<?php require "partials/head.php"?>
+<?php require "partials/nav.php"?>
+
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p class='text-3xl font-bold tracking-tight text-gray-900'>Oww!! snap, page not found</p>
+    </div>
+  </main>
+
+  <?php require "partials/footer.php"?>
